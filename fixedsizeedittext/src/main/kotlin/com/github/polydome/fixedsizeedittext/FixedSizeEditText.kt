a9a124a47e0a38662038ef4ab17add4a51f1294b
polydome <polydome@outlook.com>
2020-08-24 16:38:53 +0200

    Create FixedSizeEditText view, create demo

---
+++ fixedsizeedittext/src/main/kotlin/com/github/polydome/fixedsizeedittext/FixedSizeEditText.kt
@@ -0,0 +1,9 @@
+package com.github.polydome.fixedsizeedittext
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class FixedSizeEditText(context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
+
+}
