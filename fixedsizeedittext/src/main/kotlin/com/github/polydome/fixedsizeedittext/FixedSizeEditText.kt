9bce24608162ba1064ec099f8d100096a8d78631
polydome <polydome@outlook.com>
2020-08-25 14:36:05 +0200

    Implement characters drawing

--- fixedsizeedittext/src/main/kotlin/com/github/polydome/fixedsizeedittext/FixedSizeEditText.kt
+++ fixedsizeedittext/src/main/kotlin/com/github/polydome/fixedsizeedittext/FixedSizeEditText.kt
@@ -7,21 +7,31 @@ import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.res.ResourcesCompat
 import kotlin.math.roundToInt
 
-class FixedSizeEditText(context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
-    private val Hardcoded = object {
-        val prefs = Preferences(
+class FixedSizeEditText : AppCompatEditText {
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context): super(context)
+
+    private val HARDCODED = object {
+        val textPaint = Paint().apply {
+            color = Color.BLACK
+            textSize = 30F
+            textAlign = Paint.Align.CENTER
+        }
+
+        val preferences = Preferences(
             length = 4,
             spacing = 60,
             boxWidth = 100,
             boxHeight = 200,
             boxBackground = ResourcesCompat
-                .getDrawable(resources, R.drawable.background_box, context.theme))
+                .getDrawable(resources, R.drawable.background_box, context.theme)
+        )
     }
 
-    private val prefs = Hardcoded.prefs
+    private val prefs: Preferences = HARDCODED.preferences
     private val characters = CharArray(prefs.length)
     private val boxBackground = prefs.boxBackground
-
     private val boxRect = Rect(0, 0, 0, 0)
 
     init {
@@ -47,6 +57,7 @@ class FixedSizeEditText(context: Context, attrs: AttributeSet) : AppCompatEditTe
 
             boxBackground.bounds = boxRect
             boxBackground.draw(canvas)
+            drawCharacter(canvas, boxRect, 0)
 
             for (i in characters.indices.drop(1)) {
                 boxRect.left = (boxRect.right + prefs.spacing + 0.5).roundToInt()
@@ -54,7 +65,35 @@ class FixedSizeEditText(context: Context, attrs: AttributeSet) : AppCompatEditTe
 
                 boxBackground.bounds = boxRect
                 boxBackground.draw(canvas)
+                drawCharacter(canvas, boxRect, i)
             }
         }
     }
+
+    private fun drawCharacter(canvas: Canvas, boxRect: Rect, charIndex: Int) {
+        canvas.drawText(characters, charIndex, 1, boxRect.exactCenterX(), boxRect.exactCenterY(), HARDCODED.textPaint)
+    }
+
+    override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
+        if (isReady() && text != null) {
+            text.take(prefs.length)
+                .padEnd(prefs.length, '\u0000')
+                .forEachIndexed { index, char ->
+                    characters[index] = char
+                }
+
+            invalidate()
+        } else {
+            super.onTextChanged(text, start, lengthBefore, lengthAfter)
+        }
+    }
+
+    /**
+     * Determines whether the `super` constructor has finished and variables were initialized
+     *
+     * SENSELESS_COMPARISON is simply not true, as `onTextChanged` is called before members
+     * are initialized
+     */
+    @Suppress("SENSELESS_COMPARISON")
+    private fun isReady(): Boolean = prefs != null
 }
